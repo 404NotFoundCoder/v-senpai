7b64311dafabf95b09196121eb31db1768392d35
404NotFoundCoder <oops.shelly404@gmail.com>
2026-03-29 16:58:35 +0800

    登入加上try catch及錯誤訊息

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -12,29 +12,43 @@ export function useAuth() {
   const router = useRouter()
 
   const signInWithGitHub = async () => {
-    const result = await signInWithPopup(auth, provider)
-    const credential = GithubAuthProvider.credentialFromResult(result)
-    const accessToken = credential?.accessToken
-    const userInfo = result.user
+    try {
+      const result = await signInWithPopup(auth, provider)
+      const credential = GithubAuthProvider.credentialFromResult(result)
+      const accessToken = credential?.accessToken
+      const userInfo = result.user
 
-    const userRef = doc(db, 'users', userInfo.uid)
-    const userSnap = await getDoc(userRef)
+      const userRef = doc(db, 'users', userInfo.uid)
+      const userSnap = await getDoc(userRef)
 
-    if (userSnap.exists()) {
-      await updateDoc(userRef, { accessToken })
-    } else {
-      await setDoc(userRef, {
-        username: userInfo.displayName,
-        email: userInfo.email,
-        accessToken,
-        priority: false,
-        createdAt: new Date(),
-        role: 'user',
-      })
+      if (userSnap.exists()) {
+        await updateDoc(userRef, { accessToken })
+        console.log('exists updateUserDoc')
+      } else {
+        await setDoc(userRef, {
+          username: userInfo.displayName,
+          email: userInfo.email,
+          accessToken,
+          priority: false,
+          createdAt: new Date(),
+          role: 'user',
+        })
+        console.log('non exists setUserDoc')
+      }
+
+      user.value = userInfo
+      console.log('User 已登入:', user.value)
+    } catch (error: any) {
+      console.error('🔥 原始錯誤:', error)
+
+      if (error?.code) {
+        console.error('錯誤碼:', error.code)
+      }
+
+      if (error?.message) {
+        console.error('錯誤訊息:', error.message)
+      }
     }
-
-    user.value = userInfo
-    // console.log('User signed in:', user.value)
   }
 
   const logout = async () => {
